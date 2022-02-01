8236eec846322c6b8721bd40e7e800f785779ff3
Anish-08 <96428233+Anish-08@users.noreply.github.com>
2022-02-01 20:13:25 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("mycanvas");
-
+alert("An enhanced version of the popular game snake. Here as the game progresses obstacles are added. Earn bonus points by eating apple. Total score is the sum of size and bonus points. \n The rules are : \n  1. Control keys are W,A,S,D. \n 2. The snake dies if its head collides with the wall. \n 3. The snake grows with time, increasing the score. \n 4. You get 1/3/5 bonus points if the snake eats a fruit depending on level easy/medium/hard. \n 5. The snake dies if it collides with itself. \n 6. Hurdles appear on the board as the game progresses. \n 7. Before becoming completely solid the hurdles flicker for a short duration giving the player a buffer time to escape.\n 8. The score board appears at the end.");
 var ctx = canvas.getContext("2d");
 canvas.width = 1225;
 canvas.height = 575;
